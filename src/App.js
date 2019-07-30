5ec2f79f71feeb9d804da47e79c54684f0af8669
afranco <andrea.franco@overit.it>
2019-07-30 17:24:31 +0200

    Completed first level

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import ImageLinkForm from './components/imagelinkform/ImageLinkForm'
 import Rank from './components/rank/Rank'
 import './App.css'
 
+import { ParticleOptions } from './components/particle/ParticleOptions'
+
+//NPM parts
+import Particles from 'react-particles-js';
+
+
 class App extends Component{
 
     render() {
         return (
             <div className='App'>
+            <Particles className='particles'
+                params={ParticleOptions} />
                 <Navigation/>
                 <Logo/>
                 <Rank/>
