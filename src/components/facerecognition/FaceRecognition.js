1770a944b7338814866ab78ff97085c126023a6b
afranco <andrea.franco@overit.it>
2019-07-31 15:00:57 +0200

    Image recognition api

--- src/components/facerecognition/FaceRecognition.js
+++ src/components/facerecognition/FaceRecognition.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const FaceRegnition = ({ imageUrl }) => {
     return (
-        <div className='center'>
-            <img alt='image' src={imageUrl}/>
+        <div className='center ma '>
+            <div className='absolute mt2'>
+                {/* AUTO avoit stretch of image */}
+                <img alt='image' src={imageUrl} width='500px' height='auto'/>
+            </div>
         </div>
     )
 }
